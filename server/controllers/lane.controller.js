a522ae3735eb71d91e1dce2401cc6d04aeaf3228
Dagmara Turoń <aida7@interia.pl>
2017-08-09 00:06:06 +0200

    add new line

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -1,5 +1,23 @@
 import Lane from '../models/lane';
+import uuid from 'uuid';
 
 export function getSomething(req, res) {
   return res.status(200).end();
+}
+
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+  const newLane = new Lane(req.body);
+
+  newLane.notes = [];
+
+  newLane.id = uuid.v4();
+  newLane.save(function (err, saved) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lane: saved });
+  });
 }
